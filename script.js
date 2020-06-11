b06316648f964a83ad45cd2ca8a7c98798877b90
unknown <alextello1@hotmail.com>
2020-06-11 17:09:56 -0600

    Funcionalidad para remover transacciones

--- script.js
+++ script.js
@@ -6,37 +6,66 @@ const form = document.getElementById('form');
 const texto = document.getElementById('text');
 const cantidad = document.getElementById('amount');
 
-const dummyTransactions = [
-    { id: 1, text: 'Flores', amount: -20 },
-    { id: 2, text: 'Salario', amount: 300 },
-    { id: 3, text: 'Libro', amount: -10 },
-    { id: 4, text: 'Camara', amount: 150 },
+const dummytransacciones = [
+    { id: 1, text: 'Flores', cantidad: -20 },
+    { id: 2, text: 'Salario', cantidad: 300 },
+    { id: 3, text: 'Libro', cantidad: -10 },
+    { id: 4, text: 'Camara', cantidad: 150 },
 ];
 
-let transactions = dummyTransactions;
+let transacciones = dummytransacciones;
 
-function addTransactionDOM(transaction) {
+
+function addtransaccion(e) {
+    e.preventDefault();
+    if (texto.nodeValue?.trim() === '' || cantidad.value.trim() === '') {
+        alert('añade una cantidad');
+    } else {
+        const transaccion = {
+            id: +generarID(),
+            text: texto.value,
+            cantidad: +cantidad.value
+        };
+        transacciones.push(transaccion);
+        addtransaccionDOM(transaccion);
+        updateValues();
+        texto.value = '';
+        cantidad.value = '';
+    }
+}
+
+// Generar ID random
+function generarID() {
+    return Math.floor(Math.random() * 100000000);
+}
+
+function addtransaccionDOM(transaccion) {
     // Obtener signo
-    const sign = transaction.amount < 0 ? '-' : '+';
+    const sign = transaccion.cantidad < 0 ? '-' : '+';
 
     const item = document.createElement('li');
 
     // Añadir la clase según el valor
-    item.classList.add(transaction.amount < 0 ? 'minus' : 'plus');
+    item.classList.add(transaccion.cantidad < 0 ? 'minus' : 'plus');
 
     item.innerHTML = `
-    ${transaction.text} <span>${sign}${Math.abs(transaction.amount)}
-    <button class="delete-btn">x</button>
+    ${transaccion.text} <span>${sign}${Math.abs(transaccion.cantidad)}
+    <button class="delete-btn" onclick="removerTransaccion(${transaccion.id})">x</button>
     </span>
     `;
 
     lista.appendChild(item);
 }
 
+// Remover transaccion 
+function removerTransaccion(id) {
+    transacciones = transacciones.filter(transaccion => transaccion.id !== id);
+    init();
+}
 
 // Actalizar balance, entradas y salidaas
 function updateValues() {
-    const cantidades = transactions.map(transaction => transaction.amount);
+    const cantidades = transacciones.map(transaccion => transaccion.cantidad);
     const total = cantidades.reduce((acc, item) => (acc += item), 0).toFixed(2);
     const entradas = cantidades
         .filter(item => item > 0)
@@ -47,15 +76,17 @@ function updateValues() {
         -1
     ).toFixed(2);
     balance.innerHTML = `$${total}`;
-    moneyPlus.innerText = `${entradas}`;
-    moneyMinus.innerHTML = `${gastos}`;
+    moneyPlus.innerText = `$${entradas}`;
+    moneyMinus.innerHTML = `$${gastos}`;
 }
 // init
 
 function init() {
     lista.innerHTML = '';
-    transactions.forEach(addTransactionDOM);
+    transacciones.forEach(addtransaccionDOM);
     updateValues();
 }
 
 init();
+
+form.addEventListener('submit', addtransaccion);
